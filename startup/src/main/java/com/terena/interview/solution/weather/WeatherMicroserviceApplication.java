1707182b75d97c8483f2d65cd5821d7f9cac36a9
Manoj Kokane <manoj.kokane@amdocs.com>
2021-05-26 17:04:41 +0530

    added swagger ui and api-docs support

--- startup/src/main/java/com/terena/interview/solution/weather/WeatherMicroserviceApplication.java
+++ startup/src/main/java/com/terena/interview/solution/weather/WeatherMicroserviceApplication.java
@@ -1,13 +1,39 @@
 package com.terena.interview.solution.weather;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WeatherMicroserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherMicroserviceApplication.class, args);
+	}
+
+	@Value("${app.info.title}")
+	private String title;
+	@Value("${app.info.description}")
+	private String description;
+	@Value("${app.info.version}")
+	private String appVersion;
+	@Value("${app.info.license}")
+	private String license;
+	@Value("${app.info.contact}")
+	private String contact;
+
+	@Bean
+	public OpenAPI customOpenAPI(){
+		return new OpenAPI()
+				.info(new Info()
+				.title(title)
+				.version(appVersion)
+				.description(description)
+				.license(new License().name(license).url(contact)));
 	}
 
 }

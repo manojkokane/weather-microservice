e7cc6a6145253aeb6c896509188ae52c71fd97d7
Manoj Kokane <manoj.kokane@amdocs.com>
2021-05-26 08:20:03 +0530

    Initial setup

---
+++ startup/src/main/java/com/terena/interview/solution/weather/WeatherMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.terena.interview.solution.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherMicroserviceApplication.class, args);
+	}
+
+}
